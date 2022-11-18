6136edd3c45012db6e4183f8ff825780de237dc3
Mehdi Khadir <mehdi.khadir@hotmail.fr>
2022-11-18 12:00:03 +0100

    main: Spawn default table in front of the user if they hit the continue button

--- Assets/Modules/SceneSetup/Scripts/SceneSetupAPIController.cs
+++ Assets/Modules/SceneSetup/Scripts/SceneSetupAPIController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using Zenject;
 
@@ -6,9 +5,24 @@ public class SceneSetupAPIController : MonoBehaviour
 {
     [SerializeField] private OVRSceneManager _ovrSceneManager;
     [Inject] private ISceneSetupModel _sceneSetupModel;
+    [Header("The default table must have it's pivot on the floor")]
+    [SerializeField] private GameObject _defaultTable;
+    [SerializeField] private Vector3 defaultTableSpawningPosition = new Vector3(0, 0, 1);
 
     void Awake()
     {
         _ovrSceneManager.NoSceneModelToLoad += () => _sceneSetupModel.RaiseNoSceneModelToLoad();
+        _sceneSetupModel.SkipRoomConfiguration += OnSkipRoomConfiguration;
+    }
+
+    private void OnSkipRoomConfiguration()
+    {
+        InstantiateDefaultTable();
+    }
+
+    private void InstantiateDefaultTable()
+    {
+        var defaultTable = Instantiate(_defaultTable);
+        defaultTable.transform.position = defaultTableSpawningPosition;
     }
 }

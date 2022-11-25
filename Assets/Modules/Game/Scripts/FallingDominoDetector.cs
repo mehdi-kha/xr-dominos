2471ae7c6b000d8e7a135ce941ee8e40eac31c65
Mehdi Khadir <mehdi.khadir@hotmail.fr>
2022-11-25 15:23:37 +0100

    main: Spawn non playable dominos in a row with a hole in the sequence

--- Assets/Modules/Game/Scripts/FallingDominoDetector.cs
+++ Assets/Modules/Game/Scripts/FallingDominoDetector.cs
@@ -1,14 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FallingDominoDetector : MonoBehaviour
 {
-    [SerializeField] private string _dominoTag;
+    [SerializeField] private string _nonPlayableDominoTag = "NonPlayableDomino";
+    /// <summary>
+    ///     Contains the non playable dominos. The value is true if the domino has fallen.
+    /// </summary>
+    private Dictionary<int, bool> _fallenDominos = new();
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag != _dominoTag)
+        if (other.tag != _nonPlayableDominoTag)
         {
             return;
         }
-        Debug.Log("A domino has fallen!");
+
+        _fallenDominos[other.GetHashCode()] = true;
+
+        if (_fallenDominos.All(a => a.Value))
+        {
+            Debug.Log("All non playable dominos have fallen down!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != _nonPlayableDominoTag)
+        {
+            return;
+        }
+
+        _fallenDominos[other.GetHashCode()] = false;
     }
 }
--- Assets/Modules/Game/Scripts/NonPlayableDominosSpawner.cs
+++ Assets/Modules/Game/Scripts/NonPlayableDominosSpawner.cs
@@ -5,9 +5,13 @@ using Zenject;
 public class NonPlayableDominosSpawner : MonoBehaviour
 {
     [SerializeField] private SplineContainer _splineContainer;
-    [SerializeField] private GameObject _dominoPrefab;
+    [SerializeField] private GameObject _nonPlayableDominoPrefab;
     [SerializeField] private float _distanceBetweenDominos = 0.05f;
     [Inject] private ISceneSetupModel _sceneSetupModel;
+    [Range(0, 1)]
+    [SerializeField] private float _holeStartRelativeDistance = 0.4f;
+    [Range(0, 1)]
+    [SerializeField] private float _holeEndRelativeDistance = 0.6f;
 
     private float _splineLength;
     private float _relativeDistanceBetweenDominos;
@@ -30,10 +34,15 @@ public class NonPlayableDominosSpawner : MonoBehaviour
         while (currentRelativePosition < 1)
         {
             _splineContainer.Evaluate(currentRelativePosition, out var worldPosition, out var tangent, out var upVector);
-            var domino = Instantiate(_dominoPrefab);
+            var domino = Instantiate(_nonPlayableDominoPrefab);
+            // TODO add a non playable tag for this domino
             domino.transform.position = worldPosition;
             domino.transform.LookAt(worldPosition + tangent, upVector);
             currentRelativePosition += _relativeDistanceBetweenDominos;
+            while(currentRelativePosition > _holeStartRelativeDistance && currentRelativePosition < _holeEndRelativeDistance)
+            {
+                currentRelativePosition += _relativeDistanceBetweenDominos;
+            }
         }
     }
 }

f817b00f084fda4c9306616e7ab61ecc46e59b45
Mehdi Khadir <mehdi.khadir@hotmail.fr>
2022-11-30 11:28:41 +0100

    dissolve-hand-domino: Debug information about the IPointable events

--- Assets/Modules/Game/Scripts/Domino/DominoController.cs
+++ Assets/Modules/Game/Scripts/Domino/DominoController.cs
@@ -1,6 +1,9 @@
+using Oculus.Interaction;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(IPointable))]
 public class DominoController : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _grabInteractables;
@@ -10,6 +13,33 @@ public class DominoController : MonoBehaviour
     [SerializeField] private string _dissolveAnimationTrigger = "DissolveDomino";
     [SerializeField] private string _showupAnimationTrigger = "ShowDomino";
     [SerializeField] private Rigidbody _rigidBody;
+
+    private IPointable _pointable;
+
+    private void Awake()
+    {
+        _pointable = GetComponent<IPointable>();
+        _pointable.WhenPointerEventRaised += OnPointerEventRaised;
+    }
+
+    private void OnDestroy()
+    {
+        _pointable.WhenPointerEventRaised -= OnPointerEventRaised;
+    }
+
+    private void OnPointerEventRaised(PointerEvent pointerEvent)
+    {
+        if (pointerEvent.Type == PointerEventType.Select)
+        {
+            Debug.Log("On Select");
+        }
+
+        if (pointerEvent.Type == PointerEventType.Unselect)
+        {
+            Debug.Log("On Unselect");
+        }
+    }
+
     public void MakeNonInteractable()
     {
         foreach (var grabInteractable in _grabInteractables)
